72e9c0a17f5ffd4d39d2fcfb00aa627ab741d468
Ykong <ykonghao@icloud.com>
2022-07-06 15:23:31 +0800

    last

--- tokio-chat/src/main.rs
+++ tokio-chat/src/main.rs
@@ -1,6 +1,7 @@
 use std::error::Error;
 use tokio::io::{AsyncBufReadExt, AsyncWriteExt, BufReader};
 use tokio::net::TcpListener;
+use tokio::sync::broadcast;
 
 
 const LOCAL_SERVER: &str = "127.0.0.1:8000";
@@ -9,23 +10,44 @@ const LOCAL_SERVER: &str = "127.0.0.1:8000";
 async fn main() -> Result<(), Box<dyn Error>> {
     let listener = TcpListener::bind(LOCAL_SERVER).await?;
 
+    let (tx, _rx) = broadcast::channel(12);
+
     loop {
         let (mut socket, addr) = listener.accept().await?;
         println!("{} connected", addr);
 
+        let tx = tx.clone();
+        let mut rx = tx.subscribe();
         tokio::spawn(async move {//task
 
             let (reader, mut writer) = socket.split();
             let mut reader = BufReader::new(reader);
             let mut msg = String::new();
             loop {
-                let bytes_read = reader.read_line(&mut msg).await.unwrap();
-                if bytes_read == 0 {
-                    break;
+                // let bytes_read = reader.read_line(&mut msg).await.unwrap();
+                // if bytes_read == 0 {
+                //     break;
+                // }
+                // println!("{}", msg);
+                // writer.write_all(msg.as_bytes()).await.unwrap();
+                // msg.clear();
+                tokio::select! {
+                    result = reader.read_line(&mut msg) => {
+                        if result.unwrap() == 0 {
+                            break;
+                        }
+                        println!("{}", msg);
+                        tx.send((msg.clone(), addr)).unwrap();
+                        msg.clear();
+                    }
+                    result = rx.recv() => {
+                        let (msg_str, other_addr) = result.unwrap();
+                        if addr != other_addr {
+                            println!("{} send msg: {} to {}", other_addr, &msg_str, addr);
+                            writer.write_all(msg_str.as_bytes()).await.unwrap();
+                        }
+                    }
                 }
-                println!("{}", msg);
-                writer.write_all(msg.as_bytes()).await.unwrap();
-                msg.clear();
             }
         });
     }
--- tokio-client/src/main.rs
+++ tokio-client/src/main.rs
@@ -17,21 +17,36 @@ async fn main() -> Result<(), Box<dyn Error>> {
         let mut r = BufReader::new(r);
         let mut line = String::new();
         loop {
-            match rx.recv().await {
-                Some(msg) => {
-                    println!("msg send: {}", msg);
+            // match rx.recv().await {
+            //     Some(msg) => {
+            //         println!("msg send: {}", msg);
+            //         w.write_all(msg.as_bytes()).await.unwrap();
+            //     }
+            //     None => {}
+            // }
+            // let bytes_read = r.read_line(&mut line).await.unwrap();
+            // if bytes_read != 0 {
+            //     println!("recv :{}", line);
+            // }
+
+            tokio::select! {
+                result = r.read_line(&mut line) => {
+                    if result.unwrap() == 0 {
+                        break;
+                    }
+                    println!("recv msg: {}", line);
+                    line.clear();
+                }
+                result = rx.recv() => {
+                    let msg = result.unwrap();
+                    println!("send msg: {}", msg);
                     w.write_all(msg.as_bytes()).await.unwrap();
                 }
-                None => {}
-            }
-            let bytes_read = r.read_line(&mut line).await.unwrap();
-            if bytes_read != 0 {
-                println!("recv :{}", line);
             }
         }
     });
 
-    println!("msg: ");
+    println!("send msg: ");
     loop {
         let mut buff = String::new();
         io::stdin().read_line(&mut buff).expect("reading from stdin failed");
